4faea778e2c9abc22dd4da5b7521a32f2346920f
Rafael <rafael.suguiura@gmail.com>
2010-10-26 23:09:59 -0200

    new views

--- db/views.sql
+++ db/views.sql
@@ -1,10 +1,9 @@
-
 CREATE VIEW "ReleasesView" AS
 SELECT
-	Releases.id as id,
-	Releases.version as version,
-	Architectures.name as architecture,
-	Distributions.name as distribution
+  Releases.id as id,
+  Releases.version as version,
+  Architectures.name as architecture,
+  Distributions.name as distribution
 FROM
   Releases,
   Architectures,
@@ -13,12 +12,36 @@ WHERE
   Releases.architecture_id = Architectures.id and
   Releases.distribution_id = Distributions.id;
 
-CREATE VIEW "SelectorsView" AS
+CREATE VIEW "SelectorsCategoryView" AS
 SELECT
-	Selectors.id as id,
-	Selectors.name as name,
-	Selectors.version as version,
-	Categories.name as category
+  Selectors.id as id,
+  Selectors.name as name,
+  Selectors.version as version,
+  Categories.name as category
 FROM Selectors, Categories
 WHERE
-	Selectors.category_id = Categories.id;
+  Selectors.category_id = Categories.id;
+
+CREATE VIEW "ProjectSelectorsView" AS
+SELECT
+  Projects.name as name,
+  Projects.id as project_id,
+  Selectors.id as selector_id
+FROM
+  Selectors
+JOIN
+  Projects
+USING
+  (name);
+
+CREATE VIEW "ProjectStatisticsView" AS
+SELECT
+  ProjectSelectorsView.project_id as id,
+  ProjectSelectorsView.name as name,
+  SelectorStatistics.installations as installations
+FROM
+  ProjectSelectorsView
+JOIN
+  SelectorStatistics
+USING
+  (selector_id);
